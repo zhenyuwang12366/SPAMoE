b04b16bb883ca55af448d867eeea8413e0a0e825
KanameYuiki <zhenyuwang12366@outlook.com>
2026-01-19 12:03:22 +0800

    bug fix

--- scripts/train_seismic_moe.py
+++ scripts/train_seismic_moe.py
@@ -1641,6 +1641,9 @@ def run_inference(n_args):
                     z, _, _ = encoder_model(inputs)
                 encoded_cpu = z.detach().cpu()  # [B,C,H,W]
 
+            if input_inverse_transform is not None:
+                encoded_cpu = input_inverse_transform(encoded_cpu)
+            
             # --- 插值 u_int：bilinear resize 到 (H,W) ---
             H, W = preds_cpu.shape[-2], preds_cpu.shape[-1]
             u_int = F.interpolate(
